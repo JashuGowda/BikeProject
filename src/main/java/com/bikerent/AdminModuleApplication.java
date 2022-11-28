a9a8bf86e97264986b90420b8a2ff3604fccf3c3
JashuGowda <jashu812000@gmail.com>
2022-11-28 16:59:03 +0530

    added tomcat dependency

--- src/main/java/com/bikerent/AdminModuleApplication.java
+++ src/main/java/com/bikerent/AdminModuleApplication.java
@@ -2,10 +2,11 @@ package com.bikerent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class AdminModuleApplication {
+public class AdminModuleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminModuleApplication.class, args);
