cd8628a9ab6f4ec1aa770533c9ee0b8cfc1e8616
JashuGowda <jashu812000@gmail.com>
2022-11-28 15:18:21 +0530

    deleted generate value

--- src/main/java/com/bikerent/entities/Feedback.java
+++ src/main/java/com/bikerent/entities/Feedback.java
@@ -2,8 +2,6 @@ package com.bikerent.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -14,7 +12,6 @@ public class Feedback {
 
 	@Id
 	@Column(name = "feedback_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	@NotNull
 	@NotEmpty
